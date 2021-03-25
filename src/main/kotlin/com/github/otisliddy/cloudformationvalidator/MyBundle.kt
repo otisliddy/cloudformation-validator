1e9c643e169b5ec0991b65ab2c962cd9c2972dde
GitHub Action <action@github.com>
2021-03-25 18:09:15 +0000

    Template cleanup

--- src/main/kotlin/com/github/otisliddy/cloudformationvalidator/MyBundle.kt
+++ src/main/kotlin/com/github/otisliddy/cloudformationvalidator/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.otisliddy.cloudformationvalidator
 
 import com.intellij.AbstractBundle
 import org.jetbrains.annotations.NonNls
---
+++ src/main/kotlin/com/github/otisliddy/cloudformationvalidator/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.otisliddy.cloudformationvalidator.services
+
+import com.github.otisliddy.cloudformationvalidator.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
